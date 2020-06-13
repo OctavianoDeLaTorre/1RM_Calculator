9a286b1bba9a053e98d0e3a941d5ae52a3c8129d
Octaviano De La Torre <octavianodelatorre@gmail.com>
2020-06-13 14:12:44 -0500

    Create welcome_dialog.xml

--- app/src/main/java/com/octaviano/rm/FirstFragment.kt
+++ app/src/main/java/com/octaviano/rm/FirstFragment.kt
@@ -1,12 +1,10 @@
 package com.octaviano.rm
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -18,14 +16,7 @@ class FirstFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_homet, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
-    }
 }
